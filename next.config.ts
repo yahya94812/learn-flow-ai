26f504c273b839ad7e2c9222cbfa628a450b26ae
Yahya <155522208+yahya94812@users.noreply.github.com>
2025-10-27 14:54:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+ // ⚠️ Warning: This allows production builds to complete even if there are type errors.
+    ignoreBuildErrors: true,
 };
 
 export default nextConfig;
